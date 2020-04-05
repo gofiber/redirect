20d952879f29fe7f54e676c01b95ea65dee09296
renanbastos93 <renanbastos.tec@gmail.com>
2020-04-05 03:33:41 -0300

    Create a base module to redirect
    Created a base module to redirect based on rules
    defined by the client, even thou don't set status
    code will be used 302, so if don't set URL to redirect,
    will be to get header location to redirect.

--- README.md
+++ README.md
@@ -5,5 +5,35 @@ go get -u github.com/gofiber/redirect
 ```
 ### Example
 ```go
-...
+package main
+import (
+  "github.com/gofiber/fiber"
+  "github.com/gofiber/redirect"
+)
+func main() {
+  app := fiber.New()
+  
+  app.Use(redirect.New(redirect.Config{
+    Rules: map[string]string{
+      "/old":   redirect.Rule{RedirectTo: "google.com", StatusCode: 301},
+      "/old/*": redirect.Rule{RedirectTo: "fiber.wiki", StatusCode: 307},
+    },
+  }))
+  
+  app.Get("/new", func(c *fiber.Ctx) {
+    c.Send("Hello, World!")
+  })
+  app.Get("/new/*", func(c *fiber.Ctx) {
+    c.Send("Wildcard: ", c.Params("*"))
+  })
+  
+  app.Listen(3000)
+}
+
+```
+
+### Test
+```curl
+curl http://localhost:3000/old
+curl http://localhost:3000/old/hello
 ```
--- main.go
+++ main.go
@@ -3,3 +3,53 @@
 // 📝 Github Repository: https://github.com/gofiber/fiber
 
 package redirect
+
+import "github.com/gofiber/fiber"
+
+// Config ...
+type Config struct {
+	Filter func(*fiber.Ctx) bool // Default: nil
+	Rules  map[string]Rule       // The key we should be URL that you wish redirect
+}
+
+// Rule ...
+type Rule struct {
+	RedirectTo string
+	StatusCode int // Default: 302
+}
+
+// New ...
+func New(config ...Config) func(*fiber.Ctx) {
+	var cfg Config
+	if len(config) > 0 {
+		cfg = config[0]
+	}
+	statusCode := 302
+	for _, v := range cfg.Rules {
+		if v.StatusCode != statusCode {
+			v.StatusCode = statusCode
+		}
+	}
+
+	return func(c *fiber.Ctx) {
+		if cfg.Filter != nil && cfg.Filter(c) {
+			c.Next()
+			return
+		}
+		for k, v := range cfg.Rules {
+			if c.Path() == k {
+				if v.RedirectTo != "" {
+					c.Redirect(v.RedirectTo, v.StatusCode)
+					return
+				}
+				location := c.Get("Location")
+				if location != "" {
+					c.Redirect(location, v.StatusCode)
+					return
+				}
+			}
+		}
+		c.Next()
+		return
+	}
+}
